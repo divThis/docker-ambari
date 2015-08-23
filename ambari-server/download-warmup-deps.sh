57d56c3a8f07bda644009fbe4e5cd9c01af42814
Christopher M. Mitchell <christopher@divthis.com>
2015-08-23 16:47:29 -0700

    Updating HDP repos, adding packages to warmup and adding blueprints.

--- ambari-server/download-warmup-deps.sh
+++ ambari-server/download-warmup-deps.sh
@@ -4,6 +4,6 @@
 
 function download_deps {
   yum install -y yum-plugin-downloadonly
-  yum install -y --downloadonly hadoop_2_2_* slider_2_2_* storm_2_2_* hadoop_2_2_*-yarn hadoop_2_2_*-mapreduce snappy snappy-devel hadoop_2_2_*-libhdfs ambari-log4j falcon_2_2_* flume_2_2_* httpd python-rrdtool-1.4.5 libganglia-3.5.0-99 ganglia-devel-3.5.0-99 ganglia-gmetad-3.5.0-99 ganglia-web-3.5.7-99.noarch ganglia-gmond-3.5.0-99 ganglia-gmond-modules-python-3.5.0-99 hbase_2_2_* hive_2_2_* mysql mysql-server kafka_2_2_* knox_2_2_* extjs oozie_2_2_* pig_2_2_* sqoop_2_2_* tez_2_2_* fping nagios-plugins-1.4.9 nagios-3.5.0-99 nagios-www-3.5.0-99 nagios-devel-3.5.0-99
+  yum install -y --downloadonly hadoop_2_3_* slider_2_3_* storm_2_3_* hadoop_2_3_*-yarn hadoop_2_3_*-mapreduce snappy snappy-devel hadoop_2_3_*-libhdfs ambari-log4j falcon_2_3_* flume_2_3_* httpd python-rrdtool-1.4.5 libganglia-3.5.0-99 ganglia-devel-3.5.0-99 ganglia-gmetad-3.5.0-99 ganglia-web-3.5.7-99.noarch ganglia-gmond-3.5.0-99 ganglia-gmond-modules-python-3.5.0-99 hbase_2_3_* hive_2_3_* hive_2_3_*-hcatalog hive_2_3_*-webhcat mysql mysql-server mysql-connector-java kafka_2_3_* knox_2_3_* extjs oozie_2_3_* pig_2_3_* sqoop_2_3_* tez_2_3_* slider_2_3_* ranger_2_3_* phoenix_2_3_* storm_2_3_* kafka_2_3_* datafu_2_3_* ambari-metrics-* fping nagios-plugins-1.4.9 nagios-3.5.0-99 nagios-www-3.5.0-99 nagios-devel-3.5.0-99
 }
 trap download_deps EXIT
--- ambari-server/install-cluster.sh
+++ ambari-server/install-cluster.sh
@@ -4,6 +4,8 @@ export PATH=/usr/jdk64/jdk1.7.0_67/bin:$PATH
 
 ./ambari-shell.sh << EOF
 blueprint defaults
+blueprint add --file blueprints/single-node-lambda.json
+blueprint add --file blueprints/sandbox.json
 cluster build --blueprint $BLUEPRINT
 cluster autoAssign
 cluster create --exitOnFinish true
